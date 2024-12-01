17c91280ad66f3469c8a7ce0ea86bfc4f0b76e2b
ChstrJ <chestervjazmin@gmail.com>
2024-12-01 14:31:33 +0800

    Test

--- app/(main)/sign-up/page.tsx
+++ app/(main)/sign-up/page.tsx
@@ -1,5 +1,30 @@
+'use client'
+
+import { register } from "@/services/auth/supabaseAuth"
+import { useState } from "react"
+import { Button } from "@/components/ui/button"
+
 export default function SignUp() {
+	const [formData, setFormData] = useState({
+		email: 'cheschesj@gmail.com',
+		password: 'testtest',
+		first_name: 'chester',
+		last_name: 'test',
+		contact_number: '09123123123123',
+	})
+
+	const handleRegister = (e: any) => {
+		e.preventDefault()
+	  try {
+			const test = register(formData)
+			console.log(test)
+	  } catch (error) {
+			console.log(error)
+	  }
+	}
     return(
-        <div>Sign Up</div>
+        <div>
+			<Button onClick={(e) => {handleRegister(e)}}>Click me!</Button>
+		</div>
     )
 }
